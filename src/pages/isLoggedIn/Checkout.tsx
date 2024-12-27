f18d63d29f27d219c504e5832416b1a36c1c307c
marcusxro <marcussalopaso1@gmail.com>
2024-12-27 15:44:39 +0800

    added voucher function

--- src/pages/isLoggedIn/Checkout.tsx
+++ src/pages/isLoggedIn/Checkout.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react'
+import React, { useEffect, useState } from 'react'
 import { useNavigate, useParams } from 'react-router-dom'
 import IsLoggedIn from '../../comps/Utils/IsLoggedIn'
 import orgamixLogo from '../../assets/Orgamix.png'
@@ -6,12 +6,28 @@ import { FaInfoCircle } from "react-icons/fa";
 import { IoChevronBack } from "react-icons/io5";
 import { IoSettingsOutline } from "react-icons/io5";
 import { CiLock } from "react-icons/ci";
+import { supabase } from '../../supabase/supabaseClient';
+import Loader from '../../comps/Loader';
+
+
+interface voucherTypes {
+    id: number;
+    code: string;
+    count: number;
+    max_count: number;
+    date: string | number;
+    perc: number;
+    fixed: number;
+}
+
 
 const Checkout: React.FC = () => {
     const typeParams = useParams()
     const nav = useNavigate()
     const [user] = IsLoggedIn()
-
+    const price: number = typeParams.type === 'free' ? 0.00 : typeParams.type?.toLocaleLowerCase() === 'student' ? 50 : 100
+    const [finalPrice, setFinalPrice] = useState<number>(price)
+    const [selectedMethod, setSelectedMethod] = React.useState('gcash')
     console.log(user)
 
     useEffect(() => {
@@ -29,8 +45,81 @@ const Checkout: React.FC = () => {
     }, [typeParams])
 
 
-    const price = typeParams.type === 'free' ? '0.00' : typeParams.type?.toLocaleLowerCase() === 'student' ? '50' : '100'
-    const [selectedMethod, setSelectedMethod] = React.useState('gcash')
+    const [codeValue, setCodeValue] = useState('')
+    const [errorMessage, setErrorMessage] = useState<null | string>(null)
+    const [loading, setLoading] = useState(false)
+
+
+    const [discountInt, setDiscountInt] = useState<null | number>(0.00)
+
+    async function applyDiscount(type: string, discount: number) {
+        console.log(type, discount)
+
+        if(type === 'percentage') {
+            console.log('percentage')
+
+            const percentage = (price / 100) * discount
+            console.log(percentage)
+            setDiscountInt(percentage)
+            setFinalPrice(price - percentage)
+        }else {
+            console.log('fixed')
+            setDiscountInt(discount)
+            setFinalPrice(price - discount)
+        }
+    }
+
+
+    async function getDiscount(code: string) {
+        setLoading(true)
+        setErrorMessage(null)
+
+        if (loading) return
+
+        if (!code) {
+            setErrorMessage('Please enter a discount code')
+            setLoading(false)
+            return
+        }
+
+        try {
+            const { data: discount, error } = await supabase
+                .from('vouchers')
+                .select('*')
+                .eq('code', code)
+                .single()
+
+            if (error) {
+                setErrorMessage('Discount code not found')
+                setLoading(false)
+                throw console.error('code not found');
+            }
+
+            if (discount) {
+                setLoading(false)
+                console.log(discount)
+
+                const finalizedDiscount = discount.perc ? discount.perc : discount.fixed
+
+                const discountType = discount.perc ? 'percentage' : 'fixed'
+
+                applyDiscount(discountType, finalizedDiscount)
+
+                return discount
+            }
+        }
+        catch (error) {
+            setLoading(false)
+            setErrorMessage('Error getting discount')
+            console.error('Error getting discount:', error);
+        }
+
+    }
+
+
+
+
+
     return (
         <div className='w-full'>
             <header className='p-4 w-full flex items-center justify-between border-b-[1px] border-b-[#535353]'>
@@ -103,18 +192,35 @@ const Checkout: React.FC = () => {
                         </div>
                         <div className='flex gap-3 items-center'>
                             <input
+                                value={codeValue}
+                                onChange={(e) => setCodeValue(e.target.value)}
                                 placeholder='Enter discount code'
                                 type="text" className='p-3 w-full text-[#888] rounded-md bg-[#191919] outline-none border-[1px] border-[#535353]' />
-                            <div className='w-[300px] items-center flex gap-3 justify-center bg-[#414141] p-3 rounded-md cursor-pointer hover:bg-[#454545] border-[1px] border-[#535353]'>
-                                Apply
+                            <div
+                                onClick={() => getDiscount(codeValue)}
+                                className='w-[300px] items-center flex gap-3 justify-center bg-[#414141] p-3 rounded-md cursor-pointer hover:bg-[#454545] border-[1px] border-[#535353]'>
+                                {
+                                    loading ?
+                                        <div className='w-[20px] h-[20px]'>
+                                            <Loader />
+                                        </div>
+                                        :
+                                        'Apply'
+                                }
                             </div>
                         </div>
+                        {
+                            errorMessage &&
+                            <div className='text-red-500'>
+                                {errorMessage}
+                            </div>
+                        }
                     </div>
 
                     <div className='mt-5 flex flex-col gap-2'>
                         <div className='text-sm text-[#888]'>Subtotal       ₱{price}</div>
-                        <div className='text-sm text-[#888]'>Discount ₱0.00</div>
-                        <div className='font-semibold'>Total       ₱{price}</div>
+                        <div className='text-sm text-[#888]'>Discount ₱{discountInt}</div>
+                        <div className='font-semibold'>Total       ₱{finalPrice}</div>
                     </div>
                     <div className='border-b-[1px] border-b-[#535353] w-full mt-3'>
                     </div>
@@ -123,26 +229,26 @@ const Checkout: React.FC = () => {
                     </div>
                     <div className='mt-2 flex gap-3 items-center flex-wrap justify-start'>
 
-                        <div 
-                        onClick={() => setSelectedMethod('gcash')}
-                        className={`w-[100px]  h-[30px]   flex gap-3 items-center justify-center rounded-md overflow-hidden cursor-pointer ${selectedMethod === 'gcash' && 'border-[3px] border-green-300'} `}>
+                        <div
+                            onClick={() => setSelectedMethod('gcash')}
+                            className={`w-[100px]  h-[30px]   flex gap-3 items-center justify-center rounded-md overflow-hidden cursor-pointer ${selectedMethod === 'gcash' && 'border-[3px] border-green-300'} `}>
                             <img src="https://iqftyamgipfbmjmcqnnq.supabase.co/storage/v1/object/public/assets/checkout/gcash.png" alt="" />
                         </div>
-                        
-                        <div 
-                        onClick={() => setSelectedMethod('visa')}
-                        className={`w-[100px] h-[30px]  flex gap-3 items-center justify-center rounded-md overflow-hidden cursor-pointer ${selectedMethod === 'visa' && 'border-[3px] border-green-300'}`}>
+
+                        <div
+                            onClick={() => setSelectedMethod('visa')}
+                            className={`w-[100px] h-[30px]  flex gap-3 items-center justify-center rounded-md overflow-hidden cursor-pointer ${selectedMethod === 'visa' && 'border-[3px] border-green-300'}`}>
                             <img src="https://iqftyamgipfbmjmcqnnq.supabase.co/storage/v1/object/public/assets/checkout/visa.jpg" alt="" />
                         </div>
 
                         <div
-                        onClick={() => setSelectedMethod('bpi')}
-                         className={`w-[100px] h-[30px]  flex gap-3 items-center justify-center rounded-md overflow-hidden cursor-pointer ${selectedMethod === 'bpi' && 'border-[3px] border-green-300'}`}>
+                            onClick={() => setSelectedMethod('bpi')}
+                            className={`w-[100px] h-[30px]  flex gap-3 items-center justify-center rounded-md overflow-hidden cursor-pointer ${selectedMethod === 'bpi' && 'border-[3px] border-green-300'}`}>
                             <img src="https://iqftyamgipfbmjmcqnnq.supabase.co/storage/v1/object/public/assets/checkout/bpi.png" alt="" />
                         </div>
                         <div
                             onClick={() => setSelectedMethod('bdo')}
-                         className={`w-[100px] h-[30px]  flex gap-3 items-center justify-center rounded-md overflow-hidden cursor-pointer ${selectedMethod === 'bdo' && 'border-[3px] border-green-300'}`}>
+                            className={`w-[100px] h-[30px]  flex gap-3 items-center justify-center rounded-md overflow-hidden cursor-pointer ${selectedMethod === 'bdo' && 'border-[3px] border-green-300'}`}>
                             <img src="https://iqftyamgipfbmjmcqnnq.supabase.co/storage/v1/object/public/assets/checkout/bdo.png" alt="" />
                         </div>
                     </div>
